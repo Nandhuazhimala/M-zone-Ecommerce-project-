dae6f3da7dfe5b04d84491e666acd18ed48b29ba
Nandakumar <nandhuazhimala@gmail.com>
2025-03-18 14:08:14 +0530

    Migrate database from SQLite to PostgreSQL

--- electric_motors/electric_motors/settings.py
+++ electric_motors/electric_motors/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'electric_motors.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Mzone_project',  # database name
+        'USER': 'postgres',  
+        'PASSWORD': '123456',  
+        'HOST': 'localhost',  
+        'PORT': '5432',
     }
 }
 
